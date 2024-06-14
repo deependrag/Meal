c2851ec497b4e8c4ed4868442aa44d7761444015
Deependra Dhakal <dhakaldeependra45@gmail.com>
2024-06-14 15:47:37 -0700

    Initial Commit

---
+++ Meal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meal
+//
+//  Created by Deependra Dhakal on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
